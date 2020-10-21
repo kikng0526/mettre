39a996510ccff0e37f7a4312fe1adc144432d3e7
Kaito Yamaki <kikngo526@gmail.com>
2020-10-21 17:09:46 +0900

    commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,29 @@
 class UsersController < ApplicationController
+
   def show
     @fashions = current_user.fashions
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to users_show
+    else
+      render :show
+    end
+  end
+
+
+
+  private
+
+  def fashion_params
+    params.require(:user).permit(:username,:nickname)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  devise_for :users
+  get "users/show" => "users#show"
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   root to: "fashions#index"
-  resources :users, :only => [:show]
+  resources :users, :only => [:show, :edit, :update]
   
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
